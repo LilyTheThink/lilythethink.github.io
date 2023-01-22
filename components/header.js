a7bdc8f194c3cc01bfe197a772a519ffc2417708
TheSorcerist <mackenzie.callum@gmail.com>
2023-01-22 20:55:46 +0000

    added first content

---
+++ components/header.js
@@ -0,0 +1,43 @@
+class Header extends HTMLElement {
+    constructor() {
+      super();
+    }
+  
+    connectedCallback() {
+      this.innerHTML = `
+        
+        <header>
+            <h1 id="title"> Lily Ophelia </h1>
+            <h3> <i> pronouns she/her </i></h3>
+            <h4> "sometimes things are bad, but that's ok, because dogs still exist - alex"</h4>
+            <nav>
+                <a href="index.html">home</a>
+            </nav>
+            <hr>
+        </header>
+      `;
+    }
+  }
+
+  class Footer extends HTMLElement {
+    constructor() {
+      super();
+    }
+  
+    connectedCallback() {
+      this.innerHTML = `
+        
+        <footer>
+            <hr>
+            <nav>
+                <a href="https://youtube.com/@">youtube</a> |
+                <a href="https://github.com/LilyTheThink">github</a>
+            </nav>
+        <footer>
+      `;
+    }
+  }
+  
+  
+  customElements.define('header-component', Header);
+  customElements.define('footer-component', Footer);
