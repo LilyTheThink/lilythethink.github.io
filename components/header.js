97bfa07fb4f581b63bd9ce9fdb7d8deec65f2343
TheSorcerist <mackenzie.callum@gmail.com>
2023-01-23 21:04:23 +0000

    added badges

--- components/header.js
+++ components/header.js
@@ -33,6 +33,26 @@ class Header extends HTMLElement {
                 <a href="https://youtube.com/@lilymackenzie" target="_blank">youtube</a> |
                 <a href="https://github.com/LilyTheThink" target="_blank">github</a>
             </nav>
+            <div id="badges">
+              <img src="badges/bu12.gif" alt="this website is gay"/>
+              <a href="https://dictionary.cambridge.org/dictionary/english/gullible">
+                <img src="badges/clickhere_red.gif" />
+              </a>
+              <img src="badges/froggygif.gif" alt="little froggy"/>
+              <img src="badges/hello_kitty.gif" alt="hello kitty"/>
+              <img src="badges/minecraft.gif" alt="minecraft"/>
+              <img src="badges/spotify.gif" alt="spotify"/>
+              <img src="badges/transnow2.gif" alt="trans rights!"/>
+              <a href="https://www.twitter.com">
+                <img src="badges/twitterbutton.gif" alt="twitter"/>
+              </a>
+              <img src="badges/tf.gif" alt="music for the gays"/>
+
+              <img src="badges/tyg.gif" alt="trans your gender"/>
+              
+              <img id="phonechump" src="badges/phonechump.gif" alt="phone chump"/>
+
+            </div>
         <footer>
       `;
     }
